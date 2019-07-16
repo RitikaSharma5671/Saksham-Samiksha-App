a9669f6f6b214fbbd5dbabe60f9354891704cc37
Pranav <pranav.ps95@hotmail.com>
2019-07-16 10:05:39 +0000

    feat(SplashScreen): Adds BackendCall to fetch the forms list.
    Backend Call is RxAndroid based instead of AsyncTask

--- collect_app/src/main/java/org/odk/collect/android/ODKDriver.java
+++ collect_app/src/main/java/org/odk/collect/android/ODKDriver.java
@@ -2,4 +2,13 @@ package org.odk.collect.android;
 
 public class ODKDriver {
     public static final String LAUNCH_INTENT_ACTION = "com.psx.odk.START_ODK_APP";
+    private static int splashScreenImageRes = R.drawable.notes;
+
+    public static void init(int splashScreenImageRes) {
+        ODKDriver.splashScreenImageRes = splashScreenImageRes;
+    }
+
+    public static int getSplashScreenImageRes() {
+        return ODKDriver.splashScreenImageRes;
+    }
 }
